8baeea241d210672fbb061a73c3dd97c0fb2644a
seaerchin <jer005@e.ntu.edu.sg>
2021-10-02 18:30:48 +0800

    refactor(spec): refactor to use new runner functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Docker
 import RIO
 import qualified RIO.Map as Map
 import qualified RIO.NonEmpty.Partial as NP
+import qualified Runner
 import qualified System.Process.Typed as Process
 import Test.Hspec
 
@@ -21,37 +22,16 @@ makeStep name image commands =
 makePipeline :: Steps -> Pipeline
 makePipeline s = Pipeline {steps = s}
 
-testPipeline :: Pipeline
-testPipeline =
-  makePipeline $
-    NP.fromList
-      [ makeStep "First step" "ubuntu" ["date"],
-        makeStep "Second step" "ubuntu" ["uname -r"]
-      ]
-
-testBuild :: Build
-testBuild =
-  Build
-    { pipeline = testPipeline,
-      state = BuildReady,
-      completedSteps = mempty
-    }
-
--- | Runs a given build until the end state with the provided service
-runBuild :: Docker.Service -> Build -> IO Build
-runBuild docker build = do
-  newBuild <- Core.progress docker build
-  case newBuild.state of
-    BuildFinished br -> pure newBuild
-    _ -> do
-      -- NOTE: delay to allow the step to complete before running again.
-      -- Delay is in micro-seconds hence this delays for 1 second.
-      threadDelay (1 * 1000 * 1000)
-      runBuild docker newBuild
-
-testRunSuccess :: Docker.Service -> IO ()
-testRunSuccess docker = do
-  result <- runBuild docker testBuild
+testRunSuccess :: Runner.Service -> IO ()
+testRunSuccess runner = do
+  build <-
+    runner.prepareBuild $
+      makePipeline $
+        NP.fromList
+          [ makeStep "First step" "ubuntu" ["date"],
+            makeStep "Second step" "ubuntu" ["uname -r"]
+          ]
+  result <- runner.runBuild build
   result.state `shouldBe` BuildFinished BuildSucceeded
   Map.elems result.completedSteps `shouldBe` [StepSucceeded, StepSucceeded]
 
@@ -65,7 +45,7 @@ main :: IO ()
 main = hspec do
   -- refer here: https://hackage.haskell.org/package/hspec-2.8.3/docs/Test-Hspec.html#v:runIO
   -- This allows to run an IO action during the construction stage of the test
-  docker <- runIO Docker.createService
+  runner <- runIO $ Runner.createService =<< Docker.createService
   beforeAll cleanupDocker $ describe "Quad CI" do
     it "should run a successful build" $ do
-      testRunSuccess docker
+      testRunSuccess runner
