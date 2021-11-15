17b75a939bab480936a617b12bdd7a8ce52fad29
seaerchin <jer005@e.ntu.edu.sg>
2021-11-15 21:09:44 +0800

    feat(server): add new endpoint for github events

--- src/Server.hs
+++ src/Server.hs
@@ -1,12 +1,15 @@
 module Server where
 
 import Codec.Serialise (deserialise, serialise)
-import Core
+import qualified Core
+import qualified Data.Aeson as Aeson
+import qualified Github
 import qualified JobHandler
 import RIO
+import qualified RIO.NonEmpty as NonEmpty
 import qualified Web.Scotty as Scotty
 
-data Config = Config {port :: Int} deriving (Eq, Show)
+newtype Config = Config {port :: Int} deriving (Eq, Show)
 
 run :: Config -> JobHandler.Service -> IO ()
 run config handler =
@@ -19,3 +22,17 @@ run config handler =
       msg <- deserialise <$> Scotty.body
       Scotty.liftAndCatchIO $ handler.processMsg msg
       Scotty.json ("message procesed" :: Text)
+    Scotty.post "/webhook/github" do
+      body <- Scotty.body
+      number <- Scotty.liftAndCatchIO do
+        info <- Github.parsePushEvent (toStrictBytes body)
+        pipeline <- Github.fetchRemotePipeline info
+
+        -- create a new clone step so that we clone the repo from github before running ci commands
+        let step = Github.createCloneStep info
+
+        handler.queueJob $
+          Core.Pipeline
+            { steps = NonEmpty.cons step pipeline.steps
+            }
+      Scotty.json $ Aeson.object [("number", Aeson.toJSON $ Core.buildNumberToInt number), ("status", "job queued")]
