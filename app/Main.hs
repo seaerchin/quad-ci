fe3c16e1447562daad4b99bd83149eec5748a446
seaerchin <jer005@e.ntu.edu.sg>
2021-11-22 17:40:40 +0800

    feat(main): add main

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,24 @@
 
 module Main where
 
-import Control.Monad
+import qualified Cli
+import RIO
+import qualified System.Log.Formatter as Logger
+import qualified System.Log.Handler as Handler
+import qualified System.Log.Handler.Simple as Logger
+import qualified System.Log.Logger as Logger
 
-main :: Monad m => m ()
-main = return ()
+initLogger :: IO ()
+initLogger = do
+  logger <- Logger.getRootLogger
+  handler <- Logger.streamHandler stdout Logger.INFO
+  let formatter = Logger.simpleLogFormatter "[$time : $loggername : $prio] $msg"
+  Logger.saveGlobalLogger $
+    logger
+      & Logger.setHandlers [Handler.setFormatter handler formatter]
+      & Logger.setLevel Logger.INFO
+
+main :: IO ()
+main = do
+  initLogger
+  Cli.main
